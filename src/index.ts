c52e6a4f1b9f7b6aa4d541d80d1ea56f71503c78
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2023-05-13 23:16:01 +0200

    Scheduled support added (to create history inside KV)

--- src/index.ts
+++ src/index.ts
@@ -8,32 +8,40 @@ export interface Env {
 	RATES: KVNamespace;
 }
 
+const getData = async (env: Env) => {
+	const date = getDate();
+	const cachedRate = await env.RATES.get(date);
+
+	if (cachedRate) {
+		return new Response(cachedRate);
+	} else {
+		const response = await fetch(
+			`https://openexchangerates.org/api/latest.json?app_id=${env.API_KEY}`,
+			{
+				method: 'GET',
+			},
+		);
+
+		const data = JSON.stringify(
+			pick(await response.json(), ['timestamp', 'rates', 'base']),
+		);
+
+		await env.RATES.put(date, data);
+
+		return new Response(data);
+	}
+};
+
 export default {
+	async scheduled(event: ScheduledEvent, env: Env, ctx: ExecutionContext) {
+		return await getData(env);
+	},
+
 	async fetch(
 		request: Request,
 		env: Env,
 		ctx: ExecutionContext,
 	): Promise<Response> {
-		const date = getDate();
-		const cachedRate = await env.RATES.get(date);
-
-		if (cachedRate) {
-			return new Response(cachedRate);
-		} else {
-			const response = await fetch(
-				`https://openexchangerates.org/api/latest.json?app_id=${env.API_KEY}`,
-				{
-					method: 'GET',
-				},
-			);
-
-			const data = JSON.stringify(
-				pick(await response.json(), ['timestamp', 'rates', 'base']),
-			);
-
-			await env.RATES.put(date, data);
-
-			return new Response(data);
-		}
+		return await getData(env);
 	},
 };
