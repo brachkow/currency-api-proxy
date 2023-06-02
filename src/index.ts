07b173d01a37fb2ffc73f164a07a7b825ac9feb6
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2023-06-03 00:10:03 +0200

    Format

--- src/index.ts
+++ src/index.ts
@@ -4,44 +4,44 @@ import dayjs from 'dayjs';
 const getDate = () => dayjs().format('DD-MM-YYYY');
 
 export interface Env {
-	API_KEY: string;
-	RATES: KVNamespace;
+  API_KEY: string;
+  RATES: KVNamespace;
 }
 
 const getData = async (env: Env) => {
-	const date = getDate();
-	const cachedRate = await env.RATES.get(date);
+  const date = getDate();
+  const cachedRate = await env.RATES.get(date);
 
-	if (cachedRate) {
-		return new Response(cachedRate);
-	} else {
-		const response = await fetch(
-			`https://openexchangerates.org/api/latest.json?app_id=${env.API_KEY}`,
-			{
-				method: 'GET',
-			},
-		);
+  if (cachedRate) {
+    return new Response(cachedRate);
+  } else {
+    const response = await fetch(
+      `https://openexchangerates.org/api/latest.json?app_id=${env.API_KEY}`,
+      {
+        method: 'GET',
+      },
+    );
 
-		const data = JSON.stringify(
-			pick(await response.json(), ['timestamp', 'rates', 'base']),
-		);
+    const data = JSON.stringify(
+      pick(await response.json(), ['timestamp', 'rates', 'base']),
+    );
 
-		await env.RATES.put(date, data);
+    await env.RATES.put(date, data);
 
-		return new Response(data);
-	}
+    return new Response(data);
+  }
 };
 
 export default {
-	async scheduled(event: ScheduledEvent, env: Env, ctx: ExecutionContext) {
-		return await getData(env);
-	},
+  async scheduled(event: ScheduledEvent, env: Env, ctx: ExecutionContext) {
+    return await getData(env);
+  },
 
-	async fetch(
-		request: Request,
-		env: Env,
-		ctx: ExecutionContext,
-	): Promise<Response> {
-		return await getData(env);
-	},
+  async fetch(
+    request: Request,
+    env: Env,
+    ctx: ExecutionContext,
+  ): Promise<Response> {
+    return await getData(env);
+  },
 };
